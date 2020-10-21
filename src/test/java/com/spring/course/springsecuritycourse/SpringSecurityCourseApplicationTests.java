b0ba13cb707120feafa3f28024036b4f4691cac9
Eric Gitonga Njue <eric.gitonga@imbank.co.ke>
2020-10-21 15:46:37 +0300

    Initial Project Creation

---
+++ src/test/java/com/spring/course/springsecuritycourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.course.springsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
