57093d016715e0ebbd0eb9ce390d4cd183080bb7
Eric Gitonga Njue <eric.gitonga38@gmail.com>
2020-11-10 16:05:24 +0300

    Using PreAuthorize On Method Level & Enabling Global Security

--- src/main/java/com/spring/course/springsecuritycourse/restcontrollers/StudentManagementController.java
+++ src/main/java/com/spring/course/springsecuritycourse/restcontrollers/StudentManagementController.java
@@ -2,6 +2,7 @@ package com.spring.course.springsecuritycourse.restcontrollers;
 
 import com.spring.course.springsecuritycourse.dto.Student;
 import com.spring.course.springsecuritycourse.services.ManagementApi;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,22 +21,28 @@ public class StudentManagementController implements ManagementApi {
         students.add(new Student(900, "Pauline Wambura Njue"));
     }
 
+    // hasRole('ROLE_'), hasAnyRole('ROLE_'), hasAuthority('permission'), hasAnyAuthority('permission')
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void deleteStudent(@PathVariable String studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void updateStudent(@PathVariable Integer studentId, @RequestBody Student student) {
         System.out.println(studentId);
     }
